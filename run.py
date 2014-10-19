2e725c670e481946458dfc1d208cddd70171f884
Your Name <partin.imeri@gmail.com>
2014-10-19 12:38:24 +0200

    Initial commit

--- run.py
+++ run.py
@@ -1,24 +1,26 @@
-from flask import Flask, Response
-from bson import json_util
-from pymongo import MongoClient
+from flask import Flask
+#from flask import Response
+#from bson import json_util
+#from pymongo import MongoClient
 
 
-mongo = MongoClient()
+#mongo = MongoClient()
 
-db = mongo.gjakova
+#db = mongo.gjakova
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    # Build a list
-    # Build a dictionary
-    # if, elif, else
+    # Krijo nje liste
+
+    # Krijo nje dictionary
+
     # loop the list
+
     # loop the dictionary
-    # string operations (length, replace, concatenation)
 
-
-    return "Hello World"
+    # operacionet me string (length, replace, concatenation)
+	return "Hello World"
 
 if __name__=="__main__":
     app.run(debug=True)
