f4c7f74a112ce56641ab85cfa2c10989aa420bac
Your Name <partin.imeri@gmail.com>
2014-10-23 18:38:46 +0200

    Added Procurement and Budget Type URL Rules and logic

--- run.py
+++ run.py
@@ -28,7 +28,7 @@ def index():
 
 @app.route("/piechart/<int:viti>")
 def piechart(viti):
-    # ruajme rezultatin qe na kthehet nga databaza permes ekzekutimit te kerkeses(Query) ne json
+    # ruajme rezultatin qe na kthehet nga databaza permes ekzekutimit te kerkeses(Query) ne json.
     json = db.procurements.aggregate([
             {
                 "$match":{
@@ -64,7 +64,36 @@ def piechart(viti):
     return resp
 
 
-
+@app.route("/piechart")
+def piechart_all():
+    # ruajme rezultatin qe na kthehet nga databaza permes ekzekutimit te kerkeses(Query) ne json.
+    json = db.procurements.aggregate([
+            {
+                "$group":{
+                        "_id":{
+                            "tipi":"$tipi"
+                        },
+                        "shuma":{
+                            "$sum":"$kontrata.vlera"}
+                        }
+            },
+            {
+                "$sort":{
+                        "_id.tipi":1
+                        }
+            },
+            {
+                "$project":{
+                        "tipi":"$_id.tipi",
+                        "shuma":"$shuma",
+                        "_id":0
+                        }
+            }
+        ])
+    # pergjigjen e kthyer dhe te konvertuar ne JSON ne baze te json_util.dumps() e ruajme ne resp
+    resp = Response(response = json_util.dumps(json['result']), mimetype = 'application/json')
+    # ne momentin kur hapim  sh.: http://127.0.0.1:5000/piechart/2011 duhet te kthejme JSON, ne rastin tone resp.
+    return resp
 
 # permes app.route caktojme URL ne te cilen do te kthejme rezultatin qe na nevojitet, dhe permes <int:viti> kerkojme
 # qe te caktojme vitin ne URL per te kerkuar nga databaza te dhenat perkatese te atij viti
@@ -109,6 +138,156 @@ def treemap(viti):
     # pergjigjen e kthyer dhe te konvertuar ne JSON ne baze te json_util.dumps() e ruajme ne  resp
     resp = Response(response = json_util.dumps(json['result']), mimetype = 'application/json')
     # ne momentin kur hapim  sh.: http://127.0.0.1:5000/treemap/2011 duhet te kthejme JSON, ne rastin tone resp.
+    return resp
+
+
+@app.route("/treemap")
+def treemap_all():
+    json = db.procurements.aggregate([
+            {
+                "$group":{
+                        "_id":{
+                            "kompania":"$kompania.slug",
+                            "tipi":"$tipi"
+                        },
+                        "shuma":{
+                            "$sum":"$kontrata.vlera"
+                            }
+                        }
+            },
+            {
+                "$sort":{
+                        "_id.tipi":1,
+                        "_id.kompania":1
+                        }
+            },
+            {
+                "$project":{
+                        "kompania":"$_id.kompania",
+                        "tipi":"$_id.tipi",
+                        "shuma":"$shuma",
+                        "_id":0
+                        }
+            }
+])
+    # pergjigjen e kthyer dhe te konvertuar ne JSON ne baze te json_util.dumps() e ruajme ne  resp
+    resp = Response(response = json_util.dumps(json['result']), mimetype = 'application/json')
+    # ne momentin kur hapim  sh.: http://127.0.0.1:5000/treemap duhet te kthejme JSON, ne rastin tone resp.
+    return resp
+
+@app.route("/company/<string:name>")
+def company_details(name):
+    json = db.procurements.find(
+            {"kompania.slug":name
+
+            }
+        )
+    # pergjigjen e kthyer dhe te konvertuar ne JSON ne baze te json_util.dumps() e ruajme ne  resp
+    resp = Response(response = json_util.dumps(json), mimetype = 'application/json')
+    # ne momentin kur hapim  sh.: http://127.0.0.1:5000/treemap duhet te kthejme JSON, ne rastin tone resp.
+    return resp
+
+
+
+@app.route("/company-list")
+def company_list():
+    json = db.procurements.aggregate([
+            {
+                "$group":{
+                        "_id":{
+                            "emri":"$kompania.emri",
+                            "tipi":"$tipi",
+                            "slug":"$kompania.slug",
+                            "selia":"$kompania.selia",
+                            "viti":"$viti"
+                        },
+                        "shuma":{
+                            "$sum":"$kontrata.vlera"
+                            }
+                        }
+            },
+            {
+                "$sort":{
+                        "_id.tipi":1,
+                        "_id.kompania":1
+                        }
+            },
+            {
+                "$project":{
+                        "emri":"$_id.emri",
+                        "viti":"$_id.viti",
+                        "tipi":"$_id.tipi",
+                        "slug":"$_id.slug",
+                        "shuma":"$shuma",
+                        "_id":0
+                        }
+            }
+])
+    # pergjigjen e kthyer dhe te konvertuar ne JSON ne baze te json_util.dumps() e ruajme ne  resp
+    resp = Response(response = json_util.dumps(json['result']), mimetype = 'application/json')
+    # ne momentin kur hapim  sh.: http://127.0.0.1:5000/treemap duhet te kthejme JSON, ne rastin tone resp.
+    return resp
+
+@app.route("/budget-type")
+def budget_type():
+    # ruajme rezultatin qe na kthehet nga databaza permes ekzekutimit te kerkeses(Query) ne json.
+    json = db.procurements.aggregate([
+            {
+                "$group":{
+                        "_id":{
+                            "tipi":"$tipiBugjetit"
+                        },
+                        "shuma":{
+                            "$sum":"$kontrata.vlera"}
+                        }
+            },
+            {
+                "$sort":{
+                        "_id.tipi":1
+                        }
+            },
+            {
+                "$project":{
+                        "tipi":"$_id.tipi",
+                        "shuma":"$shuma",
+                        "_id":0
+                        }
+            }
+        ])
+    # pergjigjen e kthyer dhe te konvertuar ne JSON ne baze te json_util.dumps() e ruajme ne resp
+    resp = Response(response = json_util.dumps(json['result']), mimetype = 'application/json')
+    # ne momentin kur hapim  sh.: http://127.0.0.1:5000/piechart/2011 duhet te kthejme JSON, ne rastin tone resp.
+    return resp
+
+@app.route("/procurement-type")
+def procurement_type():
+    # ruajme rezultatin qe na kthehet nga databaza permes ekzekutimit te kerkeses(Query) ne json.
+    json = db.procurements.aggregate([
+            {
+                "$group":{
+                        "_id":{
+                            "tipi":"$tipi"
+                        },
+                        "shuma":{
+                            "$sum":"$kontrata.vlera"}
+                        }
+            },
+            {
+                "$sort":{
+                        "_id.tipi":1
+                        }
+            },
+            {
+                "$project":{
+                        "tipi":"$_id.tipi",
+                        "shuma":"$shuma",
+                        "_id":0
+                        }
+            }
+        ])
+    # pergjigjen e kthyer dhe te konvertuar ne JSON ne baze te json_util.dumps() e ruajme ne resp
+    resp = Response(response = json_util.dumps(json['result']), mimetype = 'application/json')
+    # ne momentin kur hapim  sh.: http://127.0.0.1:5000/piechart/2011 duhet te kthejme JSON, ne rastin tone resp.
     return resp
 
 if __name__ == "__main__":
