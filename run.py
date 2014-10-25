1914ed575b199a25a3c83c15f184d5bf22a85fc0
legeorges <georges.labreche@gmail.com>
2014-10-25 13:52:15 -0700

    Minor formatting fix

--- run.py
+++ run.py
@@ -32,34 +32,35 @@ def index():
 def piechart(viti):
     # ruajme rezultatin qe na kthehet nga databaza permes ekzekutimit te kerkeses(Query) ne json.
     json = db.procurements.aggregate([
-            {
-                "$match":{
-                    #bejme match ne baze te vitit te cilin e kemi marre nga URL, <int:viti>
-                        "viti":viti
-                        }
-            },
-            {
-                "$group":{
-                        "_id":{
-                            "tipi":"$tipi"
-                        },
-                        "shuma":{
-                            "$sum":"$kontrata.vlera"}
-                        }
-            },
-            {
-                "$sort":{
-                        "_id.tipi":1
-                        }
-            },
-            {
-                "$project":{
-                        "tipi":"$_id.tipi",
-                        "shuma":"$shuma",
-                        "_id":0
-                        }
+        {
+            "$match": {
+                #bejme match ne baze te vitit te cilin e kemi marre nga URL, <int:viti>
+                "viti": viti
             }
-        ])
+        },
+        {
+            "$group": {
+                "_id": {
+                    "tipi": "$tipi"
+                },
+                "shuma": {
+                    "$sum": "$kontrata.vlera"
+                }
+            }
+        },
+        {
+            "$sort": {
+                "_id.tipi": 1
+            }
+        },
+        {
+            "$project": {
+                "tipi": "$_id.tipi",
+                "shuma": "$shuma",
+                "_id": 0
+            }
+        }
+    ])
     # pergjigjen e kthyer dhe te konvertuar ne JSON ne baze te json_util.dumps() e ruajme ne resp
     resp = Response(response = json_util.dumps(json['result']), mimetype = 'application/json')
     # ne momentin kur hapim  sh.: http://127.0.0.1:5000/piechart/2011 duhet te kthejme JSON, ne rastin tone resp.
