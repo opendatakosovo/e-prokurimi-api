3b01a07ff07e80f698bd9045ba6de9bed64400c9
Georges Labreche <georges@opendatakosovo.org>
2014-10-28 16:15:03 -0700

    request for contract prices

--- run.py
+++ run.py
@@ -128,6 +128,59 @@ def treemap(viti):
     return resp
 
 
+@app.route("/treemap/price/<int:viti>")
+# krijojme funksionin treemap(viti) i cili pranon vitin nga <int:viti>
+def treemap_price(viti):
+    ''' permes app.route caktojme URL ne te cilen do te kthejme rezultatin
+        qe na nevojitet, dhe permes <int:viti> kerkojme qe te caktojme vitin
+        ne URL per te kerkuar nga databaza te dhenat perkatese te atij viti
+        Shembull : http://127.0.0.1:5000/treemap/2011
+    '''
+    json = db.procurements.aggregate([
+        {
+            "$match": {
+                    #bejme match ne baze te vitit te cilin e kemi marre nga URL, <int:viti>
+                "viti": viti
+            }
+        },
+        {
+            "$group": {
+                "_id": {
+                    "kompania": "$kompania.slug",
+                    "tipi": "$tipi"
+                },
+                "shuma": {
+                    "$sum": "$kontrata.qmimi"
+                },
+                "count": {
+                    "$sum": 1
+                }
+            }
+        },
+        {
+            "$sort": {
+                "_id.tipi": 1,
+                "_id.kompania": 1
+            }
+        },
+        {
+            "$project": {
+                "kompania": "$_id.kompania",
+                "tipi": "$_id.tipi",
+                "shuma": "$shuma",
+                "count": "$count",
+                "_id": 0
+            }
+        }
+    ])
+    # pergjigjen e kthyer dhe te konvertuar ne JSON ne baze te json_util.dumps() e ruajme ne  resp
+    resp = Response(
+        response=json_util.dumps(json['result']),
+        mimetype='application/json')
+
+    # ne momentin kur hapim  sh.: http://127.0.0.1:5000/treemap/2011 duhet te kthejme JSON, ne rastin tone resp.
+    return resp
+
 @app.route("/company/<string:name>")
 def company_details(name):
     json = db.procurements.find(
